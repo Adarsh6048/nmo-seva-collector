55ea6ce443dbb5f423bf41304840471b6fb0ae29
Adarsh6048 <adarshraj6048@gmail.com>
2026-09-16 09:47:23 +0530

    Add iOS app entry point

---
+++ ios/NMOSevaLedger/NMOSevaLedgerApp.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+@main
+struct NMOSevaLedgerApp: App {
+    @StateObject private var settings = SettingsStore()
+    @StateObject private var ledger = LedgerStore()
+
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(settings)
+                .environmentObject(ledger)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct RootView: View {
+    @EnvironmentObject private var settings: SettingsStore
+
+    var body: some View {
+        if settings.isConfigured {
+            ContentView()
+        } else {
+            SetupView()
+        }
+    }
+}
